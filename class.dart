82e065e0b04ca06150524c3f37b0256de34820c8
shivam_since2002 <sg2109964@gmail.com>
2024-04-10 22:25:41 +0530

    10 April

--- class.dart
+++ class.dart
@@ -1,3 +1,26 @@
-void main(){
-  print("Hello World");
+import 'dart:io';
+
+void main() {
+  print("Deposit a money");
+  int money = int.parse(stdin.readLineSync()!);
+
+  try{
+    DepositAmount(money);
+  }catch (e){
+    print((e as DepositErrorException).DepositErrorMessage());
+  }
+}
+
+class DepositErrorException implements Exception {
+  String DepositErrorMessage() {
+    return "Please enter a valid value";
+  }
+}
+
+void DepositAmount(int amount) {
+  if (amount < 0) {
+    throw new DepositErrorException();
+  } else {
+    print("You got it");
+  }
 }
