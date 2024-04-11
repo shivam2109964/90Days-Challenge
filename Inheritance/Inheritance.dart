a4c1114106ac5eb9512c6c106f2da215a2552d95
shivam_since2002 <sg2109964@gmail.com>
2024-04-11 13:06:15 +0530

    11 April

--- Inheritance/Inheritance.dart
+++ Inheritance/Inheritance.dart
@@ -1,22 +1,24 @@
 void main() {
-  var Dog1 = Dog("Labrado");
+  var Dog1 = Dog("Labrado", "Red");
   print("");
-  var Dog2 = Dog("Pug");
+  var Dog2 = Dog("Pug", "Black");
 }
 
 class Animal {
   String? color;
   //Default Constructor
-  Animal() {
+  Animal(String color) {
     print("Animal Default Constructor!");
+    print("The color of Animal is $color");
   }
 }
 
 class Dog extends Animal {
   String? breed;
   //Default Constructor
-  Dog(String breed) : super() {
+  Dog(String breed, String color) : super(color) {
     print("Dog Default Constructor!");
-    print("The breed of Dog is $breed");  
+    print("The breed of Dog is $breed");
+    print("The color of dog is $color");
   }
 }
