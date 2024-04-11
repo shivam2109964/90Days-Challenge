6659c8c1e2c1d72502b38be0bac41d6965238c44
shivam_since2002 <sg2109964@gmail.com>
2024-04-11 12:48:00 +0530

    11 April

---
+++ Inheritance/Inheritance.dart
@@ -0,0 +1,21 @@
+void main() {
+  var Dog1 = Dog();
+  print("");
+  var Dog2 = Dog();
+}
+
+class Animal {
+  String? color;
+  //Default Constructor
+  Animal() {
+    print("Animal Default Constructor!");
+  }
+}
+
+class Dog extends Animal { 
+  String? breed;
+  //Default Constructor
+  Dog() {
+    print("Dog Default Constructor!");
+  }
+}
