3db641a64bb2bad894e68aac5296ae8c06f339ca
shivam_since2002 <sg2109964@gmail.com>
2024-04-14 09:23:20 +0530

    14 April

---
+++ Projecty/Function/3.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+void main() {
+  print("Enter the range ");
+  int range = int.parse(stdin.readLineSync()!);
+  int sum = 0;
+
+  List<int> fibonacciSeries = generateFibonacci(range);
+
+
+
+  for (int i = 0; i < fibonacciSeries.length; i++) {
+    sum += calcualteAlltheSum(fibonacciSeries[i]);
+  }
+  print(fibonacciSeries);
+  print(sum);
+}
+
+List<int> generateFibonacci(int range) {
+  List<int> fibonacciSeries = [0, 1];
+
+  for (int i = 2; i < range; i++) {
+    fibonacciSeries.add(fibonacciSeries[i - 2] + fibonacciSeries[i - 1]);
+  }
+  return fibonacciSeries;
+}
+
+int calcualteAlltheSum(int number) {
+  int sum = 0;
+  if (number % 2 == 0) {
+    sum += number;
+  }
+  return sum;
+}
