ed31260883ed47e307b601a0117250393b44d101
shivam_since2002 <sg2109964@gmail.com>
2024-04-17 00:17:25 +0530

    17 April

--- Projecty/Opps/Associaton.dart
+++ Projecty/Opps/Associaton.dart
@@ -1,24 +1,24 @@
-// Customer class
+import 'dart:io';
+
 class Customer {
   String name;
   List<Order> orders = [];
 
   Customer(this.name);
 
-  void placeOrder(Order order) {
+  void placedOrder(Order order) {
     orders.add(order);
-    order.customer = this;
+    order.customer = this; // Association
   }
 
-  void displayOrders() {
-    print("Orders placed by $name:");
+  void displayOrder() {
+    print("Order is placed by $name");
     for (var order in orders) {
       print("- ${order.orderDetails}");
     }
   }
 }
 
-// Order class
 class Order {
   String orderDetails;
   late Customer customer;
@@ -27,19 +27,38 @@ class Order {
 }
 
 void main() {
-  // Creating customers
-  var customer1 = Customer("Alice");
-  var customer2 = Customer("Bob");
+  List<Customer> customers = [];
 
-  // Creating orders
-  var order1 = Order("Laptop");
-  var order2 = Order("Smartphone");
+  int customerCount;
+  do {
+    print("Enter the customer you want to create: ");
+    customerCount = int.parse(stdin.readLineSync()!);
+  } while (customerCount <= 0);
 
-  // Associating customers with orders
-  customer1.placeOrder(order1);
-  customer2.placeOrder(order2);
+  for (var i = 1; i <= customerCount; i++) {
+    print("Enter name for Customer $i");
+    String name = stdin.readLineSync()!;
+    var customer = Customer(name);
+    customers.add(customer);
 
-  // Displaying orders for each customer
-  customer1.displayOrders();
-  customer2.displayOrders();
+    int orderCount;
+    do {
+      print("Enter the number of order for $name");
+      orderCount = int.parse(stdin.readLineSync()!);
+    } while (orderCount <= 0);
+
+    //Creating order of each customer
+    for (var j = 1; j <= orderCount; j++) {
+      print("Enter order details for Order $j");
+      String orderDetails = stdin.readLineSync()!;
+
+      var order = Order(orderDetails);
+      customer.placedOrder(order);
+    }
+  }
+
+  //Displaying order for each customers
+  for (var customer in customers) {
+    customer.displayOrder();
+  }
 }
