3c66b58843562923045330c513a634bfe3422e93
shivam_since2002 <sg2109964@gmail.com>
2024-04-18 12:09:21 +0530

    18 April

--- Folder/map.dart
+++ Folder/map.dart
@@ -2,7 +2,7 @@ void main() {
   Map<String, int> value = {};
 
   value["Shivam"] = 2109964;
-  value["rahul"]  = 9120384;
+  value["rahul"] = 9120384;
 
-  print(value); 
+  value.containsKey(value['Shivam']);
 }
