ba8d2536ad213b23091bfcdcc440a8ef9a8147fa
shivam_since2002 <sg2109964@gmail.com>
2024-04-23 13:21:35 +0530

    23 April

--- Projecty/Prac/Suzuki.dart
+++ Projecty/Prac/Suzuki.dart
@@ -1,11 +1,37 @@
-void stairsIn20(List<List<int>> arr) {
-  List<List<int>> result = [];
-  result.addAll(arr);
-  print(arr);
+int stairsIn20(List<List<int>> arr) {
+  List<int> result = [];
+  arr.forEach((element) {
+    result.addAll(element);
+  });
+  var sumResult = sumOfAll(result);
+  return sumResult;
 }
 
-void main() {
-  List<int> day1 = [2, 4, 3], day2 = [3, 5, 8], day3 = [2, 2, 2];
+int sumOfAll(List<int> element) {
+  int sum = 0;
+  element.forEach((element) {
+    sum += element;
+  });
 
-  stairsIn20([day1, day2, day3]); 
+  return sum * 20;
+}
+
+//Using some simple value
+int sumOfll(List<List<int>> arr) =>
+    arr.expand((element) => element).reduce((v, e) => v + e) * 20;
+
+void main() {
+  List<int> arr = [2, 1, 4], arr1 = [5, 6, 7], arr2 = [8, 12, 13];
+
+  var resultCheck = stairsIn20([arr, arr1, arr2]);
+  print(resultCheck);
+
+  List<List<int>> dataOfList = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  print("---> Before Expand $dataOfList");
+  var finalSee =  dataOfList.expand((element) => element);
+  print("---> After Expand $finalSee");
 }
